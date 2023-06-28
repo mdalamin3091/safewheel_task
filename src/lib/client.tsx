ddd9aa4604be303ae9bd024264ab2abba3d02acd
mdalamin3091 <alamin@hypescout.co>
2023-06-28 16:34:08 +0600

    add env file

--- src/lib/client.tsx
+++ src/lib/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'https://graphql-pokemon2.vercel.app',
+	uri: process.env.NEXT_PUBLIC_API_URL,
 	cache: new InMemoryCache(),
 });
 
