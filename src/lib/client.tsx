ca8f0736b6e62a2c7bfba052f6b4d22d3a17fd57
Md. Al Amin <86846863+mdalamin3091@users.noreply.github.com>
2023-08-01 23:56:06 +0600

    Update client.tsx

--- src/lib/client.tsx
+++ src/lib/client.tsx
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
 	uri: process.env.NEXT_PUBLIC_API_URL,
 	cache: new InMemoryCache(),
+	 // headers:{
+  //  		authorization:localStorage.getItem("token") || ""
+  // 	}
 });
 
 export default client;
